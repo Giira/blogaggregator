2834eeb2319532a5a80dfd4d2048de7975d39f2f
Giira <euan.a.marsh@gmail.com>
2025-08-17 21:51:19 +0100

    main func blueprint

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package blogaggregator
 
-func main() {
-	return
+import (
+	"fmt"
+
+	"github.com/Giira/blogaggregator/internal/config"
+)
+
+func main() error {
+	cfg, err := config.Read()
+	if err != nil {
+		return err
+	}
+	cfg.SetUser("Euan")
+	config.Read()
+	fmt.Printf("Current user: %s\nConfig database url: %s\n", cfg.Current_user_name, cfg.Db_url)
+	return nil
 }
