fbd347527162395a899fdda15546b81601dc0cca
Giira <euan.a.marsh@gmail.com>
2025-08-28 21:54:30 +0100

    remove test in main - replace with applying config to state variable

--- main.go
+++ main.go
@@ -11,10 +11,6 @@ func main() {
 	if err != nil {
 		fmt.Printf("Error: %v", err)
 	}
-	cfg.SetUser("Euan")
-	cfg, err = config.Read()
-	if err != nil {
-		fmt.Printf("Error: %v", err)
-	}
-	fmt.Printf("Current user: %s\nConfig database url: %s\n", cfg.Current_user_name, cfg.Db_url)
+	s := state{}
+	s.cfg = &cfg
 }
