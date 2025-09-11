fdeac633e6ebce21b73402162a33a9b1b49fef59
Giira <euan.a.marsh@gmail.com>
2025-09-11 22:58:04 +0100

    feed follows table and start query

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -34,3 +34,13 @@ RETURNING *;
 SELECT feeds.name, feeds.url, users.name 
 FROM feeds, users
 WHERE feeds.user_id=users.id;  
+
+-- name: CreateFeedFollow :one
+INSERT INTO feed_follows (id, created_at, updated_at, user_id, feed_id)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5
+)
--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -15,6 +15,16 @@ CREATE TABLE feeds (
     user_id UUID references users(id) ON DELETE CASCADE NOT NULL
 );
 
+CREATE TABLE feed_follows (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    user_id UUID references users(id) ON DELETE CASCADE NOT NULL,
+    feed_id UUID references feeds(id) ON DELETE CASCADE NOT NULL,
+    UNIQUE (user_id, feed_id)
+);
+
 -- +goose Down
+DROP TABLE feed_follows;
 DROP TABLE feeds;
 DROP TABLE users;
